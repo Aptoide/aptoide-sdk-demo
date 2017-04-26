f5e8adb2b14fccbf8ed0cfd0b9a061c972f3eb2e
Neurophobic Animal <fredericosantinho@gmail.com>
2017-04-26 11:53:39 +0100

    Added valid dummy oem id.

--- app/src/main/java/cm/aptoidesdk/pt/aptoidedemo/MyTestingApp.java
+++ app/src/main/java/cm/aptoidesdk/pt/aptoidedemo/MyTestingApp.java
@@ -12,6 +12,6 @@ public class MyTestingApp extends Application {
   @Override public void onCreate() {
     super.onCreate();
 
-    Aptoide.integrate(this, "oemid");
+    Aptoide.integrate(this, "9c63f5e4892289a3e85147dede4dcb75");
   }
 }
